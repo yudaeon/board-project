5165cb4ffbfe40cf6409342285e0048bb5a695fa
yudaeon <daeon03050@gmail.com>
2023-06-21 23:48:58 +0900

    게시글 리스트 및 게시글 상세 페이지 생성

---
+++ src/main/java/com/example/board/service/BoardService.java
@@ -0,0 +1,34 @@
+package com.example.board.service;
+
+import com.example.board.entity.Board;
+import com.example.board.repository.BoardRepository;
+import jakarta.persistence.Id;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class BoardService {
+    //1. 객체선언 @Autowired 사용
+    @Autowired
+    private BoardRepository boardRepository;
+
+    // 글 작성 처리
+    public void write(Board board){ //매개변수 지정
+
+        boardRepository.save(board); //위에서 지정한 매개변수 입력
+    }
+
+    //게시글 리스트 처리
+    public List<Board> boardList(){
+
+        return boardRepository.findAll();
+    }
+
+    // 특정 게시글 불러오기
+    public Board boardView(Integer id){
+
+        return boardRepository.findById(id).get();
+    }
+}
