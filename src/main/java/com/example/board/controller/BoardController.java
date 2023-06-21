d9f880c7df113170e76794e5d302d25714a1d0f4
yudaeon <daeon03050@gmail.com>
2023-06-21 23:56:20 +0900

    게시글 삭제 구현

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -48,4 +48,10 @@ public class BoardController {
         model.addAttribute("board", boardService.boardView(id));
         return "boardview";
     }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
 }
--- src/main/java/com/example/board/service/BoardService.java
+++ src/main/java/com/example/board/service/BoardService.java
@@ -16,19 +16,21 @@ public class BoardService {
 
     // 글 작성 처리
     public void write(Board board){ //매개변수 지정
-
         boardRepository.save(board); //위에서 지정한 매개변수 입력
     }
 
     //게시글 리스트 처리
     public List<Board> boardList(){
-
         return boardRepository.findAll();
     }
 
     // 특정 게시글 불러오기
     public Board boardView(Integer id){
-
         return boardRepository.findById(id).get();
+    }
+
+    //특정 게시글 삭제
+    public void boardDelete(Integer id){
+        boardRepository.deleteById(id);
     }
 }
