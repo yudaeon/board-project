9c5a34eb036445eb16eeafa6b4228c02e8f0d5b2
yudaeon <daeon03050@gmail.com>
2023-06-21 23:48:36 +0900

    글 작성 처리

---
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -0,0 +1,51 @@
+package com.example.board.controller;
+
+
+import com.example.board.entity.Board;
+import com.example.board.service.BoardService;
+import jakarta.persistence.Id;
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@Data
+public class BoardController {
+    @Autowired
+    private BoardService boardService;
+
+    @GetMapping("/board/write")
+    public String boardWriteForm(){
+
+        return "boardwrite";
+    }
+
+    //게시글 폼
+    @PostMapping("board/writepro")
+    public String boardWritePro(Board board){
+
+        boardService.write(board);
+
+        return "";
+    }
+
+    //게시글 리스트
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list", boardService.boardList());
+
+        return "boardlist";
+    }
+
+    //상세페이지
+    @GetMapping("/board/view")  // localhost:8080/board/view?id=1
+    public String boardView(Model model, Integer id){
+
+        model.addAttribute("board", boardService.boardView(id));
+        return "boardview";
+    }
+}
