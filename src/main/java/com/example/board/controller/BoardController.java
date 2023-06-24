8117a0e2451879381d5271a8867fd4ca1f9ac7be
yudaeon <daeon03050@gmail.com>
2023-06-25 00:40:26 +0900

    게시글 파일 첨부 기능 추가

--- src/main/java/com/example/board/controller/BoardController.java
+++ src/main/java/com/example/board/controller/BoardController.java
@@ -12,6 +12,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
 
 @Controller
 @Data
@@ -27,9 +31,9 @@ public class BoardController {
 
     //게시글 폼
     @PostMapping("board/writepro")
-    public String boardWritePro(Board board, Model model){
+    public String boardWritePro(Board board, Model model, MultipartFile file) throws IOException {
 
-        boardService.write(board);
+        boardService.write(board, file);
 
         model.addAttribute("message","글 작성이 완료되었습니다.");
         model.addAttribute("searchUrl","/board/list");
@@ -60,17 +64,19 @@ public class BoardController {
     }
 
     @GetMapping("/board/modify/{id}")
-    public String boardModify(@PathVariable ("id")Integer id, Model model){
+    public String boardModify(@PathVariable ("id") Integer id, Model model){
         model.addAttribute("board", boardService.boardView(id));
         return "boardmodify";
     }
 
     @PostMapping("/board/update/{id}")
-    public String boardUpdate(@PathVariable("id") Integer id, Board board){
+    public String boardUpdate(@PathVariable("id") Integer id, Board board, MultipartFile file) throws IOException {
         //기존 글 검색
         Board boardTemp = boardService.boardView(id);
         boardTemp.setTitle(board.getTitle());
         boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp, file);
 
         return "redirect:/board/list";
     }
--- src/main/java/com/example/board/service/BoardService.java
+++ src/main/java/com/example/board/service/BoardService.java
@@ -5,8 +5,12 @@ import com.example.board.repository.BoardRepository;
 import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class BoardService {
@@ -15,7 +19,22 @@ public class BoardService {
     private BoardRepository boardRepository;
 
     // 글 작성 처리
-    public void write(Board board){ //매개변수 지정
+    public void write(Board board, MultipartFile file) throws IOException { //매개변수 지정
+        //파일 첨부 코드 추가
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files";
+
+        //파일이름을 랜덤으로 생성
+        UUID uuid = UUID.randomUUID();
+
+        //파일이름 생성
+        String fileName = uuid + "_" + file.getOriginalFilename();
+
+        //파일 생성할거고 이 경로에 이름은 이렇게 저장할거다
+        File savaFile = new File(projectPath, fileName);
+
+        //파일 저장
+        file.transferTo(savaFile);
+
         boardRepository.save(board); //위에서 지정한 매개변수 입력
     }
 
